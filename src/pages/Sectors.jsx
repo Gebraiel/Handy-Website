a8fdf1558939ffb0202a757622df745ab67a4755
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2025-01-06 11:30:31 +0200

    Update Sectors.jsx

--- src/pages/Sectors.jsx
+++ src/pages/Sectors.jsx
@@ -36,7 +36,7 @@ export default function Sectors() {
             <div className="p-5 bg-white shadow-lg text-center basis-80">
               <b className="text-xl">Capacity</b>
               <p>
-                <Counter counter="25000" duration={1000} countBy={100} />
+                <Counter counter="25000" duration={2000} countBy={100} />
                 <br/>
                 Mtons / Year
               </p>
@@ -44,7 +44,7 @@ export default function Sectors() {
             <div className="p-5 bg-white shadow-lg text-center basis-80">
               <b className="text-xl">Capacity</b>
               <p>
-                <Counter counter="800" duration={1000} countBy={10} />
+                <Counter counter="800" duration={2000} countBy={10} />
                 <br/>
                 Mtons / Year
               </p>
