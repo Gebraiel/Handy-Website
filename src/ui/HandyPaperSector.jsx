27b786360ac0aadf2f2ece134a09d3ff4c04c491
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2025-01-09 11:32:34 +0200

    .

--- src/ui/HandyPaperSector.jsx
+++ src/ui/HandyPaperSector.jsx
@@ -99,76 +99,76 @@ export default function HandyPaperSector() {
           <Paragraph size="md">
             The production of tissue paper involves several key steps:
           </Paragraph>
-          <div className="flex flex-wrap  gap-5 items-stretch ">
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary p-10 rounded-full border-2 border-secondary">
+          <div className="processes-parent flex flex-wrap  gap-5 items-stretch ">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary p-10 rounded-full border-2 border-secondary after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <FaMedal />
                   </div>
                   <h4 className="text-2xl font-bold my-5">Pulping</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                       <b>Raw Material</b> : Virgin wood pulp or recycled paper.
                   </Paragraph>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                       <b>Pulping Process</b> : The raw material is processed into
                       a fibrous slurry.
                   </Paragraph>
                 </div>
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary p-10 rounded-full border-2 border-secondary">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary p-10 rounded-full border-2 border-secondary  after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <FaThumbsUp />
                   </div>
                   <h4 className="text-2xl font-bold my-5 ">Refining</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                     <b>Fiber Modification</b> : Adjusts fiber length and
                     strength, influencing softness and absorbency.
                   </Paragraph>
                 </div>
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary p-10 rounded-full border-2 border-secondary">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary p-10 rounded-full border-2 border-secondary  after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <RiLeafFill />
                   </div>
                   <h4 className="text-2xl font-bold my-5 ">Screening</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                     <b>Impurity Removal</b> : The pulp is screened to remove
                     dirt or knots.
                   </Paragraph>
                 </div>
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary  after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <FaGlobe />
                   </div>
                   <h4 className="text-2xl font-bold my-5 ">Forming</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                     <b>Sheet Formation</b> : The pulp is spread onto a moving
                     mesh, forming a wet sheet.
                   </Paragraph>
                 </div>
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary  after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <FaGlobe />
                   </div>
                   <h4 className="text-2xl font-bold my-5 ">Pressing</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                     <b>Water Removal</b> : The wet sheet is pressed to remove
                     excess water.
                   </Paragraph>
                 </div>
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary  after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <FaGlobe />
                   </div>
                   <h4 className="text-2xl font-bold my-5 ">Drying</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                     <b>Moisture Removal</b> : The pressed sheet is dried by
                     heated cylinders.
                   </Paragraph>
                 </div>
-                <div className="lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 even:mt-10">
-                  <div className="w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary">
+                <div className="process flex-grow lg:basis-[300px] basis-[500px] text-center py-16 px-9 transition-all duration-300 ">
+                  <div className="icon w-fit m-auto text-3xl text-primary  p-10 rounded-full border-2 border-secondary  after:absolute relative after:top-0 after:right-0 after:bg-primary after:text-white after:w-8 after:h-8 after:rounded-full after:text-sm after:flex after:justify-center after:items-center">
                     <FaGlobe />
                   </div>
                   <h4 className="text-2xl font-bold my-5 ">Creping</h4>
-                  <Paragraph size="sm" className='text-left'>
+                  <Paragraph size="sm" className='lg:text-left'>
                     <b>Surface Embossing</b> : The dried paper is embossed to
                     create a soft, textured surface.
                   </Paragraph>
