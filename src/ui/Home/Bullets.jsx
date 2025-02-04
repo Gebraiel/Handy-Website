fbf9c261e204eaff8da309277c948e71bbab08e5
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2025-02-04 21:46:27 +0200

    .

--- src/ui/Home/Bullets.jsx
+++ src/ui/Home/Bullets.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
-export default function Bullets({length,active,setActive}) {
+export default function Bullets({length,active,setActive,handleClick}) {
   const [activeBullet,setActiveBullet] = useState(0);
     useEffect(()=>{
         setActiveBullet(active);
     },[active]);
   const bullets = new Array(length).fill(null);
   return (
-    bullets.map((_,index)=><button key={index} className={`w-4 h-4 rounded-full border border-white transition-colors duration-300 ${index === activeBullet ? "bg-primary" : "bg-transparent"}`} onClick={()=>setActive(index)}></button>)
+    bullets.map((_,index)=><button key={index} className={`w-4 h-4 rounded-full border border-white transition-colors duration-300 ${index === activeBullet ? "bg-primary" : "bg-transparent"}`} onClick={()=>handleClick(setActive(index))}></button>)
   )
 }
--- src/ui/Home/Slider.jsx
+++ src/ui/Home/Slider.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import nextArrow from "../../assets/icon-next.svg";
 import prevArrow from "../../assets/icon-previous.svg";
 import Typing from "./Typing";
 import Bullets from "./Bullets";
 function Slider({ slides}) {
   const [active, setActive] = useState(0);
+  // let interval = useMemo(()=>setInterval(handleNext,5000),[]);
+  let intervalRef = useRef(null)
+  const intervalTime = 5000;
+  function handleClick(fn){
+    startInterval();
+    fn();
+  }
+  function startInterval(){
+    if(intervalRef.current){
+      clearInterval(intervalRef.current)
+    }
+    intervalRef.current=setInterval(()=>{handleNext()},intervalTime);
+  }
   function handleNext() {
     setActive((active) => (active == slides.length - 1 ? 0 : active + 1));
   }
-
   function handlePrev() {
     setActive((active) => (active == 0 ? slides.length - 1 : active - 1));
   }
-
+  useEffect(()=>{
+    startInterval();
+    return ()=> clearInterval(intervalRef.current);
+  },[])
   return (
     <div className="relative h-[calc(100vh)] ">
       <div className="absolute w-full h-full left-0 top-0 bg-[#09216d] opacity-30 z-10"></div>
@@ -41,19 +56,19 @@ function Slider({ slides}) {
       <div className=" w-full hidden h-full justify-center items-end sm:flex sm:justify-between sm:items-center gap-5 pb-5 px-5 absolute left-0 top-0 z-20">
         <button
           className="bg-white w-10 h-10 flex justify-center items-center rounded-full"
-          onClick={handlePrev}
+          onClick={()=>handleClick(handlePrev)}
         >
           <img src={prevArrow} />
         </button>
         <button
           className="bg-white w-10 h-10 flex justify-center items-center rounded-full"
-          onClick={handleNext}
+          onClick={()=>handleClick(handleNext)}
         >
           <img src={nextArrow} />
         </button>
       </div>
       <div className='flex gap-5 absolute w-full bottom-5 left-0 justify-center z-50'>
-        <Bullets length={slides.length} active={active} setActive={setActive}/>
+        <Bullets length={slides.length} active={active} setActive={setActive} handleClick={handleClick}/>
       </div>
     </div>
   );
